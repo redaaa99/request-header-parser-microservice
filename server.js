90cefdc71444a05aa686c566c8f86cf8e77c71b5
System <none>
2017-08-12 00:25:57 +0000

    done

--- server.js
+++ server.js
@@ -13,10 +13,33 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
+  var ua = request.headers['user-agent'],
+    $ = {};
+
+  if (/mobile/i.test(ua))
+      $.Mobile = true;
+
+  if (/like Mac OS X/.test(ua)) {
+      $.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
+      $.iPhone = /iPhone/.test(ua);
+      $.iPad = /iPad/.test(ua);
+  }
+
+  if (/Android/.test(ua))
+      $.Android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
+
+  if (/webOS\//.test(ua))
+      $.webOS = /webOS\/([0-9\.]+)[\);]/.exec(ua)[1];
+
+  if (/(Intel|PPC) Mac OS X/.test(ua))
+      $.Mac = /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/.exec(ua)[2].replace(/_/g, '.') || true;
+
+  if (/Windows NT/.test(ua))
+      $.Windows = /Windows NT ([0-9\._]+)[\);]/.exec(ua)[1];
   response.json({
     ip_adress : request.headers['x-forwarded-for'].split(",")[0].toString(),
      language : request.headers["accept-language"].split(",")[0].toString(),
-     software : request.headers["user-agent"]..toString()
+     software : $
   });
 });
 
